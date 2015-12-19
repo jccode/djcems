fffa650540c73c9735ec42a970def0f971e4e602
jcchen <junchangchen@gmail.com>
2015-12-19 16:36:51 +0800

    django redis

--- djcems/settings.py
+++ djcems/settings.py
@@ -120,6 +120,22 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
+# Cache
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/0",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
+#
+# SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+# SESSION_CACHE_ALIAS = "default"
+
+
 # rest framework
 
 REST_FRAMEWORK = {
