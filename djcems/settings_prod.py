72c1bb38b39878640c2c08836dbe81f1523dac35
jcchen <junchangchen@gmail.com>
2015-12-28 13:46:02 +0800

    prod

--- djcems/settings_prod.py
+++ djcems/settings_prod.py
@@ -23,3 +23,24 @@ ADMINS = (
 MANAGERS = (
     ("Jessie", "492429624@qq.com"),
 )
+
+# Cache
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
+CORS_ORIGIN_WHITELIST += (
+    '112.74.93.116',
+    '10.116.12.125',
+)
